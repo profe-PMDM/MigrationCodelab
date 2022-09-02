acd7fc8f22f2556498cd84d8bc5be21cc3a52b42
David Corcuera <david.corcuera@gmail.com>
2022-09-02 18:21:13 +0200

    add DisposeOnViewTreeLifecycleDestroyed ViewCompositionStrategy

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -22,6 +22,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -110,6 +111,9 @@ class PlantDetailFragment : Fragment() {
                 }
             }
 
+            composeView.setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+            )
             composeView.setContent {
                 // Compose land
                 MaterialTheme {
